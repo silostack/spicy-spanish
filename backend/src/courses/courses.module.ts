68cafcd3bbe5cf2318a7c81ed34784805d233762
si <simon@rebelfi.io>
2026-02-25 21:01:43 -0500

    refactor: update CoursesModule for new entity registrations

--- backend/src/courses/courses.module.ts
+++ backend/src/courses/courses.module.ts
@@ -1,14 +1,13 @@
 import { Module } from '@nestjs/common';
 import { MikroOrmModule } from '@mikro-orm/nestjs';
 import { Course } from './entities/course.entity';
-import { CourseLesson } from './entities/course-lesson.entity';
-import { StudentCourse } from './entities/student-course.entity';
+import { CourseSchedule } from './entities/course-schedule.entity';
 import { CoursesController } from './courses.controller';
 import { CoursesService } from './courses.service';
 import { User } from '../users/entities/user.entity';
 
 @Module({
-  imports: [MikroOrmModule.forFeature([Course, CourseLesson, StudentCourse, User])],
+  imports: [MikroOrmModule.forFeature([Course, CourseSchedule, User])],
   controllers: [CoursesController],
   providers: [CoursesService],
   exports: [CoursesService],
